0f77e95dfc44723378a27b82b7ca349de25ccf96
sacre1noire <100489722+sacre1noire@users.noreply.github.com>
2024-12-16 19:21:52 +0300

    Delete л2з1.2.py

--- л2з1.2.py
+++
@@ -1,9 +0,0 @@
-def max_of_two(x,y):
-    if x>y: print(x)
-    if y>x: print(y)
-    if x==y: print('Числа равны')
-print('Введите число 1')
-a=int(input())
-print('Введите число 2')
-b=int(input())
-max_of_two(a,b)
